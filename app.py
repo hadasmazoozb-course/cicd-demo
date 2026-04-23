083e71c922c520592cb01985ac6b2465ea4bede2
hadasmazoozb-course <hadasmazoozb@gmail.com>
2026-04-24 01:18:46 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def calculate_area(width, height):
+    return width * height
+
+@app.route('/')
+def home():
+    return "Python CI/CD Pipeline is Live!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
